2c9d311f65e893f703600355c4b141402f0805c3
MrImaginatory <138272731+MrImaginatory@users.noreply.github.com>
2026-07-16 14:58:58 +0200

    chore: enable database logging in development and add dev origin to next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['pin-waterproof-driven-municipality.trycloudflare.com'],
 };
 
 export default nextConfig;
